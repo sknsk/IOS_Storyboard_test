95a68ab192db1e0566aaf1aaeef78b108a8ea36e
F839-019 M <mf839-019@70-12-242-53.pools.spcsdns.net>
2016-05-18 17:05:55 +0900

    Initial Commit

---
+++ PushTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushTest
+//
+//  Created by MF839-019 on 2016. 5. 18..
+//  Copyright © 2016년 MF839-019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
