476b02f9861d5a838cf1841fb450660576c858b4
MF839-019 <mf839-019@70-12-242-225.pools.spcsdns.net>
2016-05-19 11:54:54 +0900

    프로젝트 시작

--- PushTest/ViewController.swift
+++ PushTest/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func unwindToviewController(segue:UIStoryboardSegue) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
